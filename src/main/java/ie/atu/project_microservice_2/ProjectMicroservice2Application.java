4295be4a99893b8cbdc24e8c45a30e8facee9491
Mikaela-D <G00401392@atu.ie>
2024-01-10 12:03:47 +0000

    Created new project Microservice 2

---
+++ src/main/java/ie/atu/project_microservice_2/ProjectMicroservice2Application.java
@@ -0,0 +1,13 @@
+package ie.atu.project_microservice_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectMicroservice2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectMicroservice2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/project_microservice_2/ProjectMicroservice2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.project_microservice_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectMicroservice2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
